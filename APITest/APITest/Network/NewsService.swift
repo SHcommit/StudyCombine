7f02f968ce10456bd04426163daa5972de07d371
ysh <happysh_s2@naver.com>
2023-01-04 14:09:41 +0900

    기본적으로 api 파싱받는거!

--- APITest/APITest/Network/NewsService.swift
+++ APITest/APITest/Network/NewsService.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import Combine
+
+enum NewsServiceError: LocalizedError {
+    case addressUnreachable(URL)
+    case invalidResponse
+}
+extension NewsServiceError: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .invalidResponse:
+            return "The server responded with garbage"
+        case .addressUnreachable(let url):
+            return "\(url.absoluteString) is unreachable!!!!!!!!"
+        }
+    }
+}
+
+enum NewsServiceEndPoint {
+    static let baseURL = PrivateAPIs.baseURL
+    case stories
+    case story(Int)
+    
+    var url: URL {
+        switch self {
+        case .stories:
+            return NewsServiceEndPoint.baseURL.appendingPathComponent("newstories.json")
+        case .story(let id):
+            return NewsServiceEndPoint.baseURL.appendingPathComponent("item/\(id).json")
+        }
+    }
+}
+
+struct NewsService {
+    private static let decoder = JSONDecoder()
+    
+    static func story(id: Int) -> AnyPublisher<Story,NewsServiceError> {
+        return URLSession.shared
+            .dataTaskPublisher(for: NewsServiceEndPoint.story(id).url)
+            .print()
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: RunLoop.main)
+            .map(\.data)
+            .decode(type: Story.self, decoder: decoder)
+            .mapError { error -> NewsServiceError in
+                return error as? NewsServiceError ?? .invalidResponse
+            }.eraseToAnyPublisher()
+            
+        //mapError의 Error를 통한 .finish가 가능하다. .catch를 통해서 잡아줄 수도 있다.
+        //ex) .mapError 대신 .catch { _ in Empty<Story,Error>() }
+            
+    }
+}
--- APITest/APITest/ViewController.swift
+++ APITest/APITest/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    //MARK: - Constraints
+    
+    //MARK: - Properties
+    var story: Story?
+    //MARK: - Combine properties
+    var subscriptions = Set<AnyCancellable>()
+    //MARK: - Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+        fetchNewsStory(with: 1000)
+    }
 }
 
+//MARK: - API
+extension ViewController {
+    func fetchNewsStory(with id: Int)  {
+        NewsService.story(id: id)
+            .sink { state in
+                switch state {
+                case .failure(let error):
+                    print("DEBUG: \(error)")
+                case .finished:
+                    print("DEBUG: Success fetch new story.")
+                }
+                
+            } receiveValue: {
+                print($0)
+                self.story = $0
+            }.store(in: &subscriptions)
+    }
+}
