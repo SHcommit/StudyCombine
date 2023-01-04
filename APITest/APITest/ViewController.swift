f78e2c9c83600b4945433452f42870383ba77e4e
ysh <happysh_s2@naver.com>
2023-01-04 12:58:11 +0900

    Create Api test

---
+++ APITest/APITest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APITest
+//
+//  Created by 양승현 on 2023/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
